20011bcf3e7ddc58b5aea7cd3e9fd11be50102e8
Jason Miller <contact@jasonmiller.nl>
2019-11-17 00:43:25 +0100

    Update Github Actions

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,21 @@ impl SimpleState for Pong {}
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
     
-    // Display configuration
     let app_root = application_root_dir()?;
-    let display_config_path = app_root.join("config").join("display.ron");
+
+    // Display configuration
+    let display_config = app_root.join("config").join("display.ron");
 
     let assets_dir = app_root.join("assets");
 
     // Application setup
-    let game_data = GameDataBuilder::default().with_bundle(
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
             // The RenderToWindow plugin provides all the scaffolding for opening a window and
             // drawing it
             .with_plugin(
-                RenderToWindow::from_config_path(display_config_path)
+                RenderToWindow::from_config_path(display_config)
                     .with_clear([0.0, 0.0, 0.0, 1.0]),
             )
             // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
@@ -36,6 +38,7 @@ fn main() -> amethyst::Result<()> {
 
     let mut game = Application::new(assets_dir, Pong, game_data)?;
     game.run();
+
     Ok(())
 }
 
